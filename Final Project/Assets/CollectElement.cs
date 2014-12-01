ed5ee738381fd3730bde1b26df84df3f0286facd
Dustin Taylor <taylordusting@gmail.com>
2014-12-01 16:02:59 -0700

    Added particle emitter activation upon element collection

--- Final Project/Assets/CollectElement.cs
+++ Final Project/Assets/CollectElement.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class CollectElement : MonoBehaviour {
 
+
+	public ParticleSystem system;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -15,7 +18,17 @@ public class CollectElement : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D col)
 	{
-		if (col.gameObject.tag == "Player") 
+		if (col.gameObject.tag == "Player") {
 			Debug.Log ("Collected " + this.gameObject.name);
+			StartCoroutine( Wait() );
+			this.gameObject.renderer.enabled = false;
+		}
+	}
+
+	IEnumerator Wait() {
+		system.Play ();
+		yield return new WaitForSeconds(.25f);
+		system.Stop (); 
+		Destroy(this.gameObject);
 	}
 }
