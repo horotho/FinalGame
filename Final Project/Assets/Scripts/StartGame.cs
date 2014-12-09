eebd3390f9c0b2c9c94d9c8129b1e6541efd75c1
Dustin Taylor <taylordusting@gmail.com>
2014-12-09 14:37:28 -0700

    Added an Instructions Scene to the Main UI w/ rough control outline

--- Final Project/Assets/Scripts/StartGame.cs
+++ Final Project/Assets/Scripts/StartGame.cs
@@ -18,6 +18,18 @@ public class StartGame : MonoBehaviour {
 	public void StartGamePlay()
 	{
 		GameObject player = GameObject.FindGameObjectWithTag("Player");
+		ScreenFade.Instance.Fade(2, 1, player.GetComponent<Controller>());
+	}
+
+	public void StartInstructions()
+	{
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
 		ScreenFade.Instance.Fade(1, 1, player.GetComponent<Controller>());
+	}
+
+	public void GoMainMenu()
+	{
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+		ScreenFade.Instance.Fade(0, 1, player.GetComponent<Controller>());
 	}
 }
